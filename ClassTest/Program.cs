eb5dd4850e35de8020ed6a09cefa69228cf47119
Sofa S <ssofi0101@gmail.com>
2019-09-16 19:54:11 +0300

    Добавьте файлы проекта.

---
+++ ClassTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassTest
+{
+    class Day
+    {
+        public int number;//число
+        public string month;//месяц
+        public Day( int n, string m) { number = n; month = m; }
+    }
+    class Exact:Day
+    {
+        public string name;//день недели
+        public Exact(string name, int number, string month) : base(number, month)
+        {
+            this.name = name;
+           
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Число: {number} Месяц: {month} День недели:{name}");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exact fifteenseptember = new Exact("Sunday", 15, "September");
+            fifteenseptember.GetInfo();
+        }
+    }
+}
